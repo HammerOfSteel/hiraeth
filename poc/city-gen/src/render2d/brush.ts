1e765de9bddf16146ddeb9b02e9505a1ea161742
HammerOfSteel <erikstalhammar@gmail.com>
2026-07-11 16:30:26 +0200

    feat(r2d): Brush constants

---
+++ poc/city-gen/src/render2d/brush.ts
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: GPL-3.0
+// Port of com.watabou.towngenerator.mapping.Brush
+
+export const NORMAL_STROKE = 0.3
+export const THICK_STROKE  = 1.8
+export const THIN_STROKE   = 0.15
+
+export const TOWER_RADIUS  = 1.5
+export const GATE_TICK     = 2.0
